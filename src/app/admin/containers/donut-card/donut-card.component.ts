655068270fe1071edc7a48b38e53af5aa0de877b
wadpas <wadpas@gmail.com>
2023-02-09 21:27:21 +0200

    added css styling

---
+++ src/app/admin/containers/donut-card/donut-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+import { Donut } from '../../models/donut.model';
+
+@Component({
+  selector: 'app-donut-card',
+  templateUrl: './donut-card.component.html',
+  styleUrls: ['./donut-card.component.css']
+})
+export class DonutCardComponent {
+  @Input() donut!: Donut;
+
+  constructor() { }
+
+
+}
